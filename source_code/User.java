92e8e0879e884c987627b9600c917ea8a8109352
nasirahmedsuvo <mdshuvo2361@gmail.com>
2024-11-10 09:30:53 -0800

    update code

--- source_code/User.java
+++ source_code/User.java
@@ -3,4 +3,15 @@ public class User{
     private String name;
     private String email;
     private String password;
+
+    public User(int userID, String name, String email, String password){
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void Resister(){
+        System.out.println("User's Resistration Complete");
+    }
 }

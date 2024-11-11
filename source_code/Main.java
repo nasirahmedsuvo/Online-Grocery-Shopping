3f67d3cdffc73456770acee7f4cf5d8f9d650ad0
nasirahmedsuvo <mdshuvo2361@gmail.com>
2024-11-10 19:41:33 -0800

    added user

--- source_code/Main.java
+++ source_code/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        User user = new User(1, "Robert Downey Jr", "rdj@gmail.com", "I'mIronMan");
+        user.Resister();
     }
 }

aba3ec2e343e39293b976a1bc49e74bc7bd8111a
S M Najmul Hossain <smnajmulhossain6@gmail.com>
2024-11-11 13:58:17 +0600

    Creating Product

--- source_code/Main.java
+++ source_code/Main.java
@@ -6,6 +6,19 @@ public class Main {
         if(user.Login("rdj@gmail.com", "I'mIronMan")){
             System.out.println("User Logged In");
         }
+
+
+
+
+        //Product 
+
+        // Creating instances of Product
+        Product product1 = new Product(1, "Laptop", 999.99f, "Electronics", 10);
+        Product product2 = new Product(2, "Smartphone", 599.99f, "Electronics", 20);
+
+
+
+
     }
 
     

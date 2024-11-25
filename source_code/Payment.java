9ac007a299518c491e068a18ab868e2199cef942
Sudip Mandal <sudip7981@gmail.com>
2024-11-25 18:58:33 +0600

    Payment method added

--- source_code/Payment.java
+++ source_code/Payment.java
@@ -9,9 +9,9 @@ public class Payment {
         this.method = method;
     }
 
-
-    public static void main(String[] args) {
-        
+    public void processPayment() {
+        System.out.println("Payment processed successfully with method: " + method);
     }
+
     
 }

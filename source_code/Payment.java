4b314f87655364575db34b0eafb549e4333883c3
Sudip Mandal <sudip7981@gmail.com>
2024-11-25 18:43:13 +0600

    attributes assignment

--- source_code/Payment.java
+++ source_code/Payment.java
@@ -3,6 +3,12 @@ public class Payment {
     private float amount;
     private String method;
 
+    public Payment(int paymentId, float amount, String method) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.method = method;
+    }
+
 
     public static void main(String[] args) {
         

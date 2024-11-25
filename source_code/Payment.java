a08ae864ce174ef7b2d2cd6b274333b40bdbab79
Sudip Mandal <sudip7981@gmail.com>
2024-11-25 18:39:43 +0600

    Payment class - attributes only

---
+++ source_code/Payment.java
@@ -0,0 +1,11 @@
+public class Payment {
+    private int paymentId;
+    private float amount;
+    private String method;
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
